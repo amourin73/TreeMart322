d8124941e100b66ee9c4b8eb34a27d5cdcaf9cfb
amourin73 <22101064@uap-bd.edu>
2025-02-09 19:59:54 +0600

    homepage review

---
+++ myproject/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
